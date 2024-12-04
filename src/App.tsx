c839f1f1fdaae5994499f3a964e2761c9d627b3f
marwen <marwen.mrabti.me@gmail.com>
2024-12-04 11:58:47 +0100

    feat: responsive views and icons grid in xl views

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import { EpicStackLogo, logos } from "./assets/logos/logos";
+import { cn } from "./lib/utils";
+
+//! dynamic naming of classes {`xl:col-start-${i}`} doesn't work in tailwind so we need to create a map for the dynamic classes and use cn to merge them with the static classes
+const columnClasses: Record<(typeof logos)[number]["column"], string> = {
+  1: "xl:col-start-1",
+  2: "xl:col-start-2",
+  3: "xl:col-start-3",
+  4: "xl:col-start-4",
+  5: "xl:col-start-5",
+};
+
+const rowClasses: Record<(typeof logos)[number]["row"], string> = {
+  1: "xl:row-start-1",
+  2: "xl:row-start-2",
+  3: "xl:row-start-3",
+  4: "xl:row-start-4",
+  5: "xl:row-start-5",
+  6: "xl:row-start-6",
+};
 
 function App() {
   return (
     <main className="grid min-h-dvh max-w-dvw place-items-center overflow-x-hidden bg-transparent px-4 py-16">
-      <div className="grid gap-12">
-        <div className="flex w-full max-w-md flex-col items-center text-center">
+      <div className="grid place-items-center gap-12 sm:gap-16 xl:grid-cols-[auto_1fr] xl:gap-24">
+        <div className="flex w-full max-w-md flex-col items-center text-center xl:order-2 xl:items-start xl:text-left">
           <EpicStackLogo className="size-20" />
 
-          <h1 className="mt-6 text-4xl font-medium text-black">
+          <h1 className="sm:text-4.5xl lg:text-5.5xl mt-6 text-4xl font-medium text-black md:mt-8 md:text-5xl">
             The <span className="text-highlight">Epic</span> Stack
           </h1>
-          <p className="mt-4 text-base text-slate-600">
+          <p className="mt-4 text-base text-slate-600 sm:text-lg md:mt-6 md:text-xl">
             Check the{" "}
             <a href="#" className="text-black underline underline-offset-2">
               getting started
@@ -19,11 +38,15 @@ function App() {
           </p>
         </div>
 
-        <ul className="flex w-full max-w-3xl flex-wrap items-center justify-center gap-2">
-          {logos.map(({ src, alt, href }) => (
+        <ul className="flex w-full max-w-3xl flex-wrap items-center justify-center gap-2 sm:gap-4 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6">
+          {logos.map(({ src, alt, href, column, row }) => (
             <li
               key={href}
-              className="bg-highlight/7 shadow-highlight/50 size-20 overflow-hidden rounded-2xl border shadow-md"
+              className={cn(
+                "bg-highlight/7 shadow-highlight/50 size-20 overflow-hidden rounded-2xl shadow-md sm:size-24",
+                columnClasses[column],
+                rowClasses[row],
+              )}
             >
               <a
                 href={href}
--- src/assets/logos/logos.ts
+++ src/assets/logos/logos.ts
@@ -1,108 +1,138 @@
-import docker from './docker.svg'
-import eslint from './eslint.svg'
-import github from './github.svg'
-import msw from './msw.svg'
-import playwright from './playwright.svg'
-import prettier from './prettier.svg'
-import prisma from './prisma.svg'
-import remix from './remix.svg'
-import resend from './resend.svg'
-import sentry from './sentry.svg'
-import shadcnUI from './shadcn-ui.svg'
-import sqlite from './sqlite.svg'
-import tailwind from './tailwind.svg'
-import typescript from './typescript.svg'
-import vitest from './vitest.svg'
-import zod from './zod.svg'
+import docker from "./docker.svg";
+import eslint from "./eslint.svg";
+import github from "./github.svg";
+import msw from "./msw.svg";
+import playwright from "./playwright.svg";
+import prettier from "./prettier.svg";
+import prisma from "./prisma.svg";
+import remix from "./remix.svg";
+import resend from "./resend.svg";
+import sentry from "./sentry.svg";
+import shadcnUI from "./shadcn-ui.svg";
+import sqlite from "./sqlite.svg";
+import tailwind from "./tailwind.svg";
+import typescript from "./typescript.svg";
+import vitest from "./vitest.svg";
+import zod from "./zod.svg";
 
-export { EpicStackLogo } from './epic-stack'
+export { EpicStackLogo } from "./epic-stack";
 
-/*
-
-*/
 export const logos = [
-	{
-		src: remix,
-		alt: 'Remix',
-		href: 'https://remix.run',
-	},
+  {
+    src: remix,
+    alt: "Remix",
+    href: "https://remix.run",
+    column: 1,
+    row: 1,
+  },
+  {
+    src: sqlite,
+    alt: "SQLite",
+    href: "https://sqlite.org",
+    column: 1,
+    row: 2,
+  },
+  {
+    src: prisma,
+    alt: "Prisma",
+    href: "https://prisma.io",
+    column: 1,
+    row: 3,
+  },
 
-	{
-		src: sqlite,
-		alt: 'SQLite',
-		href: 'https://sqlite.org',
-	},
-	{
-		src: prisma,
-		alt: 'Prisma',
-		href: 'https://prisma.io',
-	},
-	{
-		src: zod,
-		alt: 'Zod',
-		href: 'https://zod.dev/',
-	},
-	{
-		src: github,
-		alt: 'GitHub',
-		href: 'https://github.com',
-	},
-	{
-		src: resend,
-		alt: 'Resend',
-		href: 'https://resend.com',
-	},
+  {
+    src: zod,
+    alt: "Zod",
+    href: "https://zod.dev/",
+    column: 2,
+    row: 2,
+  },
+  {
+    src: github,
+    alt: "GitHub",
+    href: "https://github.com",
+    column: 2,
+    row: 3,
+  },
+  {
+    src: resend,
+    alt: "Resend",
+    href: "https://resend.com",
+    column: 2,
+    row: 4,
+  },
 
-	{
-		src: tailwind,
-		alt: 'Tailwind CSS',
-		href: 'https://tailwindcss.com',
-	},
+  {
+    src: tailwind,
+    alt: "Tailwind CSS",
+    href: "https://tailwindcss.com",
+    column: 3,
+    row: 3,
+  },
 
-	{
-		src: shadcnUI,
-		alt: 'shadcn/ui',
-		href: 'https://ui.shadcn.com/',
-	},
-	{
-		src: playwright,
-		alt: 'Playwright',
-		href: 'https://playwright.dev/',
-	},
-	{
-		src: msw,
-		alt: 'MSW',
-		href: 'https://mswjs.io',
-	},
-	{
-		src: vitest,
-		alt: 'Vitest',
-		href: 'https://vitest.dev',
-	},
+  {
+    src: shadcnUI,
+    alt: "shadcn/ui",
+    href: "https://ui.shadcn.com/",
+    column: 3,
+    row: 4,
+  },
+  {
+    src: playwright,
+    alt: "Playwright",
+    href: "https://playwright.dev/",
+    column: 3,
+    row: 5,
+  },
+  {
+    src: msw,
+    alt: "MSW",
+    href: "https://mswjs.io",
+    column: 3,
+    row: 6,
+  },
 
-	{
-		src: docker,
-		alt: 'Docker',
-		href: 'https://www.docker.com',
-	},
-	{
-		src: typescript,
-		alt: 'TypeScript',
-		href: 'https://typescriptlang.org',
-	},
-	{
-		src: prettier,
-		alt: 'Prettier',
-		href: 'https://prettier.io',
-	},
-	{
-		src: eslint,
-		alt: 'ESLint',
-		href: 'https://eslint.org',
-	},
-	{
-		src: sentry,
-		alt: 'Sentry',
-		href: 'https://sentry.io',
-	},
-] as const
+  {
+    src: prettier,
+    alt: "Prettier",
+    href: "https://prettier.io",
+    column: 4,
+    row: 2,
+  },
+  {
+    src: vitest,
+    alt: "Vitest",
+    href: "https://vitest.dev",
+    column: 4,
+    row: 3,
+  },
+  {
+    src: docker,
+    alt: "Docker",
+    href: "https://www.docker.com",
+    column: 4,
+    row: 4,
+  },
+  {
+    src: typescript,
+    alt: "TypeScript",
+    href: "https://typescriptlang.org",
+    column: 4,
+    row: 5,
+  },
+
+  {
+    src: eslint,
+    alt: "ESLint",
+    href: "https://eslint.org",
+    column: 5,
+    row: 1,
+  },
+  {
+    src: sentry,
+    alt: "Sentry",
+    href: "https://sentry.io",
+    column: 5,
+    row: 2,
+  },
+] as const;
