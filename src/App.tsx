828678162fc8d9ae317dfb310172e66e5c961127
marwen <marwen.mrabti.me@gmail.com>
2024-12-04 09:30:57 +0100

    feat: mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
-import { Fragment } from "react/jsx-runtime";
 import { EpicStackLogo, logos } from "./assets/logos/logos";
 
 function App() {
   return (
-    <Fragment>
-      {/*  *👀 */}
-      <main className="bg-highlight-7 min-h-dvh max-w-dvw space-y-4 px-4 py-2">
-        <EpicStackLogo className="size-20" />
-        <h1 className="text-4xl">
-          The <span className="text-highlight">Epic</span> Stack
-        </h1>
-        <p className="text-base text-slate-600">
-          Check the{" "}
-          <a href="#" className="text-black underline">
-            getting started
-          </a>{" "}
-          guide file for how to get your project off the ground!
-        </p>
-        <ul className="grid grid-cols-3 place-items-center gap-4">
+    <main className="grid min-h-dvh max-w-dvw place-items-center overflow-x-hidden bg-transparent px-4 py-16">
+      <div className="grid gap-12">
+        <div className="flex w-full max-w-md flex-col items-center text-center">
+          <EpicStackLogo className="size-20" />
+
+          <h1 className="mt-6 text-4xl font-medium text-black">
+            The <span className="text-highlight">Epic</span> Stack
+          </h1>
+          <p className="mt-4 text-base text-slate-600">
+            Check the{" "}
+            <a href="#" className="text-black underline underline-offset-2">
+              getting started
+            </a>{" "}
+            guide file for how to get your project off the ground!
+          </p>
+        </div>
+
+        <ul className="flex w-full max-w-3xl flex-wrap items-center justify-center gap-2">
           {logos.map(({ src, alt, href }) => (
             <li
               key={href}
-              className="aspect-square w-16 overflow-hidden rounded-lg border-1 p-2"
+              className="bg-highlight/7 shadow-highlight/50 size-20 overflow-hidden rounded-2xl border shadow-md"
             >
               <a
                 href={href}
-                className="flex h-full w-full items-center justify-center"
+                className="grid h-full w-full place-items-center p-4"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -34,8 +36,8 @@ function App() {
             </li>
           ))}
         </ul>
-      </main>
-    </Fragment>
+      </div>
+    </main>
   );
 }
 
